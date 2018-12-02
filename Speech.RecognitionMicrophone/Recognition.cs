ce0ffdfd8d234b7cc3409c834db61cc32e4ee216
Anouar <Anouar.ben.zahra@hotmail.com>
2018-12-02 18:30:53 +0100

    first comit

---
+++ Speech.RecognitionMicrophone/Recognition.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speech.RecognitionMicrophone
+{
+    public class Recognition
+    {
+        //config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+        public async Task<string> GetTextFromMicrophone(SpeechConfig config)
+        {           
+            string result = string.Empty;     
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                var recognizerAsync = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+
+                if (recognizerAsync.Reason == ResultReason.RecognizedSpeech)
+                    result = recognizerAsync.Text;
+
+                else if (recognizerAsync.Reason == ResultReason.NoMatch)
+                    result = "Error";
+
+                else if (recognizerAsync.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(recognizerAsync);                    
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        result = "Cancellation Error";                       
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
